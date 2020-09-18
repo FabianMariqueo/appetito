cd9cd7e3a873d5bc69c92ad3ee6e8b8d053bdb06
Fabián <Mariqueo>
2020-09-18 01:27:48 -0300

    Agregando carusel para imagenes

--- lib/src/pages/add-recipe/add_recipe.dart
+++ lib/src/pages/add-recipe/add_recipe.dart
@@ -1,5 +1,5 @@
-import 'dart:io';
 
+import 'package:appetito/src/pages/add-recipe/recipe_images.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -23,78 +23,7 @@ class _AddRecipePage extends State<AddRecipePage> {
       appBar: AppBar(
         title: Text('Agregar Receta'),
       ),
-      body: Container(
-        padding: EdgeInsets.all(30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            OutlineButton(
-              onPressed: chooseImage,
-              child: Text('Choose Image'),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            showImage(),
-            SizedBox(
-              height: 20.0,
-            ),
-            OutlineButton(
-              onPressed: () {},
-              child: Text('Upload Image'),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              status,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.green,
-                fontWeight: FontWeight.w500,
-                fontSize: 20.0,
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  chooseImage() {
-    setState(() {
-      file = ImagePicker().getImage(source: ImageSource.gallery);
-    });
-  }
-
-  Widget showImage() {
-    return FutureBuilder<PickedFile>(
-      future: file,
-      builder: (BuildContext context, AsyncSnapshot<PickedFile> snapshot) {
-        if (snapshot.connectionState == ConnectionState.done &&
-            null != snapshot.data) {
-          tmpFile = snapshot.data;
-          return Flexible(
-            child: Image.file(
-              File(snapshot.data.path),
-              fit: BoxFit.fill,
-            ),
-          );
-        } else if (null != snapshot.error) {
-          return const Text(
-            'Error Picking Image',
-            textAlign: TextAlign.center,
-          );
-        } else {
-          return const Text(
-            'No Image Selected',
-            textAlign: TextAlign.center,
-          );
-        }
-      },
+      body: Container(padding: EdgeInsets.all(30.0), child: RecipeImages()),
     );
   }
 }
