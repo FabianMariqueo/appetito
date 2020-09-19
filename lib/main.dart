257b9dd3d93a338d1cb8e2ba22f9eaca1a3c0818
Fabián <Mariqueo>
2020-09-18 22:36:06 -0300

    Correccion error al cargar inicialmente (Firebase initialize)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:appetito/src/pages/app.dart';
 import 'package:appetito/src/services/auth.dart';
+import 'package:appetito/src/shared/loading.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:appetito/src/routes/routes.dart';
@@ -17,19 +18,39 @@ class MyApp extends StatelessWidget {
     return FutureBuilder<FirebaseApp>(
         future: Firebase.initializeApp(),
         builder: (BuildContext context, AsyncSnapshot<FirebaseApp> snapshot) {
+          // Si el status esta aun cargando
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return this._loadingApp();
+          }
+
+          // Estado listo de la application
           return StreamProvider<UserAppetito>.value(
-              value: AuthService().user,
-              child: MaterialApp(
-                debugShowCheckedModeBanner: false,
-                title: 'Appetito',
-                theme: ThemeData(
-                  primarySwatch: Colors.amber,
-                  visualDensity: VisualDensity.adaptivePlatformDensity,
-                ),
-                //home: AppPage(),
-                initialRoute: AppPage.tag,
-                routes: getRoutes(),
-              ));
+              value: AuthService().user, child: this._mainApp());
         });
+  }
+
+  Widget _mainApp() {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Appetito',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      //home: AppPage(),
+      initialRoute: AppPage.tag,
+      routes: getRoutes(),
+    );
+  }
+
+  Widget _loadingApp() {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Appetito',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Loading());
   }
 }
