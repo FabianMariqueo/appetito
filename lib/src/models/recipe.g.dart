4a3f8e8203a9482936620f63270e30e09e4f3119
Fabián <Mariqueo>
2020-09-22 00:21:03 -0300

    Agregar imagenes en cloud storage

--- lib/src/models/recipe.g.dart
+++ lib/src/models/recipe.g.dart
@@ -8,11 +8,12 @@ part of 'recipe.dart';
 
 Recipe _$RecipeFromJson(Map<String, dynamic> json) {
   return Recipe(
+    id: json['id'] as String,
+    userId: json['userId'] as String,
     name: json['name'] as String,
     description: json['description'] as String,
     portions: json['portions'] as int,
     procedures: (json['procedures'] as List)?.map((e) => e as String)?.toList(),
-    imagesFiles: json['imagesFiles'] as List,
     imagesURL: (json['imagesURL'] as List)?.map((e) => e as String)?.toList(),
     ingredients:
         (json['ingredients'] as List)?.map((e) => e as String)?.toList(),
@@ -23,11 +24,12 @@ Recipe _$RecipeFromJson(Map<String, dynamic> json) {
 }
 
 Map<String, dynamic> _$RecipeToJson(Recipe instance) => <String, dynamic>{
+      'id': instance.id,
+      'userId': instance.userId,
       'name': instance.name,
       'description': instance.description,
       'portions': instance.portions,
       'preparationTime': instance.preparationTime?.toIso8601String(),
-      'imagesFiles': instance.imagesFiles,
       'imagesURL': instance.imagesURL,
       'ingredients': instance.ingredients,
       'procedures': instance.procedures,
